86ad66d27d0332b941ea67e444e64d0b67e23175
Ekram Hosen <ekramhosen329@gmail.com>
2022-07-19 11:39:48 +0600

    navbar icon and profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,60 @@ import avatar from "../data/avatar.jpg";
 import { Cart, Chat, Notification, userProfile } from ".";
 import { useStateContext } from '../context/ContextProvider';
 
+const NavButton = ({ title, customFunc, icon, color, dotcolor }) => (
+    <TooltipComponent content={title} position="BottomCenter">
+        <button type="button" onClick={ customFunc } style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
+            <span style={{ background: dotcolor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
+                {icon}
+            </span>
+        </button>
+    </TooltipComponent>
+)
+
 
 const Navbar = () => {
+    const { activeMenu, setActiveMenu } = useStateContext();
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className="flex justify-between p-2 md:mx-6 relative">
+            <NavButton 
+                title="Menu" 
+                customFunc={() => 
+                setActiveMenu(prevActiveMenu => !prevActiveMenu)} 
+                color="blue" 
+                icon={<AiOutlineMenu />} 
+            />
+            <div className="flex">
+            <NavButton 
+                title="Cart" 
+                customFunc={() => {}} 
+                color="blue" 
+                icon={<FiShoppingCart />} 
+            />
+            <NavButton 
+                title="Chat"
+                dotcolor="#03C9D7"
+                customFunc={() => {}} 
+                color="blue" 
+                icon={<BsChatLeft />} 
+            />
+            <NavButton 
+                title="Notification"
+                dotcolor="#03C9D7"
+                customFunc={() => {}} 
+                color="blue" 
+                icon={<RiNotification3Line />} 
+            />
+            <TooltipComponent content="Profile" position="BottomCenter">
+                <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => {}}>
+                    <img src={avatar} alt="" className="rounded-full w-8 h-8" />
+                    <p>
+                        <span className="text-gray-400 text-14">Hi, </span>
+                        <span className="text-gray-400 font-bold ml-1 text-14">Jonas</span>
+                    </p>
+                    <MdKeyboardArrowDown className="text-gray-400 text-14" />
+                </div>
+            </TooltipComponent>
+            </div>
         </div>
     );
 };
