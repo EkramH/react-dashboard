8e22b4c6124040c83758f4d61567395e73048b70
Ekram Hosen <ekramhosen329@gmail.com>
2022-07-20 19:13:16 +0600

    ecommenrce earnign data

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ color, bgColor, text, borderRadius, size }) => {
     return (
-        <div>
-            <h2>Button</h2>
-        </div>
+        <button type='button' style={{ backgroundColor: bgColor, color, borderRadius}} className={`text-${size} p-3 hover:drop-shadow-xl`}>
+            {text}
+        </button>
     );
 };
 
