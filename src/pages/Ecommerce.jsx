29f41b3bf84442dc7b478fa0b06d2a1ad408604b
Ekram Hosen <ekramhosen329@gmail.com>
2022-07-26 14:04:00 +0600

    theme complete

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,6 +6,7 @@ import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
 import { useStateContext } from '../context/ContextProvider';
 
 const Ecommerce = () => {
+    const { currentColor } = useStateContext();
     return (
         <div className='mt-12'>
             <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -17,7 +18,7 @@ const Ecommerce = () => {
                         </div>
                     </div>
                     <div className='mt-6'>
-                        <Button color='white' bgColor='blue' text='Download' borderRadius='8px' size='md' />
+                        <Button color='white' bgColor={currentColor} text='Download' borderRadius='8px' size='md' />
                     </div>
                 </div>
                 <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
@@ -70,10 +71,10 @@ const Ecommerce = () => {
                                 <p className='text-gray-500 mt-1'>Expense</p>
                             </div>
                             <div className='mt-5'>
-                            <SparkLine currentColor="blue" id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+                            <SparkLine currentColor={currentColor} id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} />
                             </div>
                             <div className='mt-10'>
-                                <Button color='white' bgColor='blue' text='Downlaod Report' borderRadius='10px'>                                    
+                                <Button color='white' bgColor={currentColor} text='Downlaod Report' borderRadius='10px'>                                    
                                 </Button>
                             </div>
                         </div>
